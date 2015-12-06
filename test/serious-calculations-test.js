35eff5bc2d96764c369965b28af4310f60b00ee6
junjunjam <junjunjam@gmail.com>
2015-12-07 01:34:41 +0900

    no message

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -1,14 +1,10 @@
-var assert = require('assert'),
-    vows = require('vows'),
-    seriousCalculations = require('../');
 
-vows.describe('serious-calculations').addBatch({
-  'When performing serious calculations': {
-    topic: seriousCalculations.performSeriousCalculations(4),
-    'result should be valid': function (result) {
-      assert.isNumber(result);
-      assert.equal(result, 8);
-    }
-  }
-}).export(module);
-
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
